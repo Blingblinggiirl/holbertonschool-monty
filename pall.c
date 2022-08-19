724d528e8562847004ca9a9552c8570f79951947
EmilianoJZX100 <5083@holbertonschool.com>
2022-08-19 11:10:12 -0700

    opcode pall

--- pall.c
+++ pall.c
@@ -1,7 +1,17 @@
 #include "monty.h"
 /**
- *
+ * pall - prints all the values from the nodes in the stack
+ * @stack: pointer to 1st node
+ * @line_number: reads the line from the file
  */
 void pall(stack_t **stack, unsigned int line_number)
 {
+	stack_t *h = **stack;
 
+	while (h)
+	{
+		printf("%d\n", h->n);
+		counter++;
+		h = h->next;
+	}
+}
